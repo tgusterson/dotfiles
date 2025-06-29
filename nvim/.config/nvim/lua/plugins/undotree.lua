39a6afa426199e596e6be2289891dba737029770
Thom Gusterson <thom@proworkflow.com>
2025-06-29 14:01:51 +1200

    misc nvim config tidying

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -2,7 +2,7 @@ return {
 	"jiaoshijie/undotree",
 	dependencies = "nvim-lua/plenary.nvim",
 	config = true,
-	keys = { -- load the plugin only when using it's keybinding:
+	keys = { -- load the plugin only when using its keybinding:
 		{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle [U]ndotree" },
 	},
 }
